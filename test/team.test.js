1cde31a9dff29457388a61268a98d8453ca94abd
Gordon-Magill <gordon.magill@gmail.com>
2022-10-09 00:01:50 -0700

    Added basic Team tests

--- test/team.test.js
+++ test/team.test.js
@@ -1,18 +1,36 @@
-const Team = require('../lib/Team')
+const Team = require("../lib/Team");
+const inquirer = require('inquirer')
+// const fs = require("fs");
+
+
+jest.mock("inquirer");
 
 describe("Team", () => {
-    describe('Initialization', () => {
-        it('Should inherit from Team', () => {
-            let obj = new Team();
-            expect(obj instanceof Team).toEqual(true);
-        })
+  describe("Initialization", () => {
+    it("Should inherit from Team", () => {
+      let obj = new Team();
+      expect(obj instanceof Team).toEqual(true);
+    });
 
-        it('Should initialize with correct properties', () => {
-            obj = new Team("Employee's name", "id#123456", 'tests@email.com', 'test-gitName');
-            expect(obj.getName()).toEqual("Employee's name")
-            expect(obj.getID()).toEqual("id#123456")
-            expect(obj.getEmail()).toEqual('tests@email.com')
-            expect(obj.getRole()).toEqual('Employee')
-        })
-    })
-})
+    it("Should initialize with correct properties", () => {
+      obj = new Team();
+      expect(obj.getManager()).toEqual(null);
+      expect(obj.getTeamRoster()).toEqual([]);
+    });
+  });
+
+//   describe("beginQuestions", () => {
+//     it("Should trigger inquirer prompt", () => {
+//         obj = new Team()
+
+//       const mock = jest.spyOn(obj, "log");
+//       mock.mockImplementation(() => {});
+
+//       log.black(message);
+
+//       expect(mock).toBeCalledWith(colors.black, message);
+
+//       mock.mockRestore();
+//     });
+//   });
+});
